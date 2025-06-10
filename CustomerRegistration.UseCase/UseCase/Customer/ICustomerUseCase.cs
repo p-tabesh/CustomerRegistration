66647f7f303ababd6316024a2b73c35ff9aa0765
p-tabesh <pooyatabeshomom@gmail.com>
2025-06-10 21:38:23 +0330

    init commit

---
+++ CustomerRegistration.UseCase/UseCase/Customer/ICustomerUseCase.cs
@@ -0,0 +1,10 @@
+﻿using CustomerRegistration.UseCase.Entity;
+
+namespace CustomerRegistration.UseCase.UseCase.Customer;
+
+public interface ICustomerUseCase
+{
+    void Register();
+    void ChangePhoneNumber();
+    void ChangeEmail();
+}
